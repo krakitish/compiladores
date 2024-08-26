da809728dfd64d103ee1f2a5ca06663f83e5ea72
krakitish <117325072+krakitish@users.noreply.github.com>
2024-08-26 10:42:22 -0600

    Add files via upload
    Problema 2

---
+++ Problema2.java
@@ -0,0 +1,62 @@
+
+
+package Problema2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problema2 {
+    
+    static int palabra=0;
+    static int nEntero=0;
+    static int compuesto=0;
+    
+    static void tp(char[] c){
+        
+        int letra=0;
+        int numero=0;
+        int espacio=0;
+        int signo=0;
+        
+        for (char ch :c) {
+            if(Character.isLetter(ch))
+            {letra++;
+            }else if(Character.isSpaceChar(ch)){
+            espacio++;
+            }else if(Character.isDigit(ch)){
+            numero++;
+            }else{
+            signo++;
+            }
+            }
+        
+        System.out.print(c);
+        
+        if (letra>0&&numero==0) {
+                 System.out.print("(Palabra) ");
+                 palabra++;
+            } else if (letra==0&&numero>0) {
+                 System.out.print("(Numero entero) ");
+                 nEntero++;
+            }
+             else  {
+                System.out.print("(Compuesto) ");
+                compuesto++;
+            }
+        
+    }
+    
+  
+    public static void main(String[] args) {
+       Scanner entrada=new Scanner(System.in);
+       String texto=entrada.nextLine();
+       String[] palabras=texto.split(" ");
+       
+        for (String s:palabras) {
+            tp(s.toCharArray());
+        }
+       System.out .println("\nNo. Total:"+(palabra+nEntero+compuesto)+" Palabras:"+palabra+" No. Enteros:"+nEntero+" Palabras compuestas:"+compuesto);
+       
+    }
+}
+    
